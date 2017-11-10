0a3cac5be14da6095592f9387a6bf6cd2da98258
aikawasatoshi <aikawasatoshi@aikawasatoshi-no-MacBook-Pro.local>
2017-11-10 13:12:34 +0800

    =added accouts-ui and related functions

--- image_share.js
+++ image_share.js
@@ -6,10 +6,26 @@ if (Meteor.isClient) {
     Images.find({}, {sort:{createdOn: -1, rating:-1}})
   });
 
+    Template.body.helpers({username:function(){
+
+      console.log("HEY here!!"+Meteor.user());
+      console.log(Meteor.user().emails[0].address);
+
+      if(Meteor.user()){
+        return Meteor.user().emails[0].address;
+      }else{
+        return " anonymous user";
+      }
+
+      return " dunno who are you...";
+    }
+
+
+    });
    Template.images.events({
     'click .js-image':function(event){
         $(event.target).css("width", "50px");
-    }, 
+    },
     'click .js-del-image':function(event){
        var image_id = this._id;
        console.log(image_id);
@@ -17,17 +33,17 @@ if (Meteor.isClient) {
        // then remove it at the end of the animation
        $("#"+image_id).hide('slow', function(){
         Images.remove({"_id":image_id});
-       })  
-    }, 
+       })
+    },
     'click .js-rate-image':function(event){
       var rating = $(event.currentTarget).data("userrating");
       console.log(rating);
       var image_id = this.id;
       console.log(image_id);
 
-      Images.update({_id:image_id}, 
+      Images.update({_id:image_id},
                     {$set: {rating:rating}});
-    }, 
+    },
     'click .js-show-image-form':function(event){
       $("#image_add_form").modal('show');
     }
@@ -42,8 +58,8 @@ if (Meteor.isClient) {
       console.log("src: "+img_src+" alt:"+img_alt);
 
       Images.insert({
-        img_src:img_src, 
-        img_alt:img_alt, 
+        img_src:img_src,
+        img_alt:img_alt,
         createdOn:new Date()
       });
        $("#image_add_form").modal('show');
@@ -53,4 +69,3 @@ if (Meteor.isClient) {
 
 
 }
-
