146a6ddea4cce69b1e81db52b00745b73a96a900
aikawasatoshi <aikawasatoshi@aikawasatoshi-no-MacBook-Pro.local>
2017-11-17 21:03:47 +0800

    =tidy up codes

---
+++ sem2/client/main.js
@@ -0,0 +1,90 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.searchResult.helpers({
+  tasks: [
+    {
+      toppic:"TOPPIC0",
+      packageName:"INGREDIENTS0",
+      description:"DIRECTIONS",
+      createdAt: "2017/11/16",//new Date(), // current time
+      likeCount:0,
+      likes: [],
+      owner: "aikawa",//Meteor.userId(),
+      username:"ai",// Meteor.user().username,
+    },
+    {
+      toppic:"TOPPIC0",
+      packageName:"INGREDIENTS1",
+      description:"DIRECTIONS",
+      createdAt: "2017/11/16",//new Date(), // current time
+      likeCount:0,
+      likes: [],
+      owner: "aikawa",//Meteor.userId(),
+      username:"ai",// Meteor.user().username,
+    },
+    {
+      toppic:"TOPPIC0",
+      packageName:"INGREDIENTS2",
+      description:"DIRECTIONS",
+      createdAt: "2017/11/16",//new Date(), // current time
+      likeCount:0,
+      likes: [],
+      owner: "aikawa",//Meteor.userId(),
+      username:"ai",// Meteor.user().username,
+    },
+    {
+      toppic:"TOPPIC0",
+      packageName:"INGREDIENTS3",
+      description:"DIRECTIONS",
+      createdAt: "2017/11/16",//new Date(), // current time
+      likeCount:0,
+      likes: [],
+      owner: "aikawa",//Meteor.userId(),
+      username:"ai",// Meteor.user().username,
+    },
+    {
+      toppic:"TOPPIC0",
+      packageName:"INGREDIENTS4",
+      description:"DIRECTIONS",
+      createdAt: "2017/11/16",//new Date(), // current time
+      likeCount:0,
+      likes: [],
+      owner: "aikawa",//Meteor.userId(),
+      username:"ai",// Meteor.user().username,
+    },
+    {
+      toppic:"TOPPIC0",
+      packageName:"INGREDIENTS5",
+      description:"DIRECTIONS",
+      createdAt: "2017/11/16",//new Date(), // current time
+      likeCount:0,
+      likes: [],
+      owner: "aikawa",//Meteor.userId(),
+      username:"ai",// Meteor.user().username,
+    },
+  ],
+});
+
+
+
+Template.toppic.helpers({
+  date:function(){
+    this.createdAt;
+  },
+  username:function(){
+    return this.username;
+  },
+  likes:function(){
+    return this.likes.length;
+  },
+});
+
+
+//ui pointing secondary "menu"
+//"item" active
+$( document ).ready(function() {
+  $('.menu .item').tab();
+});
