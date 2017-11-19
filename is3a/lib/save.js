f5500cf5cb30393b6bd22a0e90f29afc02fdd283
aikawasatoshi <aikawasatoshi@aikawasatoshi-no-MacBook-Pro.local>
2017-11-19 23:01:05 +0800

    deleted unused files

--- is3a/lib/save.js
+++
@@ -1,6 +0,0 @@
-SavePackage = function(name, data) {
-  data = _.clone(data);
-  delete data._id;
-  data.stars = (data.repoInfo)? data.repoInfo.stars : 0;
-  Tasks.update(name, {$set: data}, {upsert: true});
-};
